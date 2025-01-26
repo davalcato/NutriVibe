848c2388a5a8c1797021b0c82c1b25a3377fcfeb
Daval <davalcato@yahoo.com>
2025-01-25 17:27:03 -0800

    Initial Commit

---
+++ NutriVibe/NutriVibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriVibeApp.swift
+//  NutriVibe
+//
+//  Created by Ethan Hunt on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct NutriVibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NutriVibeTests/NutriVibeTests.swift
@@ -0,0 +1,16 @@
+//
+//  NutriVibeTests.swift
+//  NutriVibeTests
+//
+//  Created by Ethan Hunt on 1/25/25.
+//
+
+import Testing
+
+struct NutriVibeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
